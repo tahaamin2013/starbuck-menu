3b09b3c989cd2fb5b876782ca5d4ddb75828064b
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-03 15:28:03 +0500

    add some drinks

---
+++ lib/menuItems.ts
@@ -0,0 +1,28 @@
+// menuItems.js
+export const menuItems = [
+  {
+    category: "Drinks",
+    items: [
+      {
+        name: "Hot Coffees",
+        image: "/hot cofee.avif",
+        link: "hello",
+      },
+      {
+        name: "Cold Coffees",
+        image: "/cold cofee.avif",
+        link: "hello",
+      },
+      {
+        name: "Hot Coffees",
+        image: "/hot cofee.avif",
+        link: "hello",
+      },
+      {
+        name: "Cold Coffees",
+        image: "/cold cofee.avif",
+        link: "hello",
+      },
+    ],
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,43 @@
 import Sidebar from "@/components/sidebar";
 import Image from "next/image";
 import Link from "next/link";
+import { menuItems } from "../../lib/menuItems"; // Adjust the path according to your directory structure
 
 export default function Home() {
   return (
     <div className="flex px-5 md:px-[90px] my-9">
       <div>
         <Sidebar />
-      </div>{" "}
+      </div>
       <div>
         <h1 className="font-bold text-2xl">Menu</h1>
 
-        <div>
-          <div className="mt-[36px]">
-            <h1 className="font-bold text-2xl mb-4">Drinks</h1>
-            <div className="flex flex-col md:flex-row gap-[40px] md:gap-[100px]">
-              <div className="flex gap-8 flex-col md:flex-row">
-                <Link href="hello" className="flex flex-row items-center gap-5">
-                  <Image
-                    src="/hot cofee.avif"
-                    alt="hot cofee"
-                    width={120}
-                    height={120}
-                    className="rounded-full"
-                  ></Image>
-                  <div>
-                    <h1 className="text-xl">Hot Coffees</h1>
-                  </div>
-                </Link>
-              </div>
-              <div className="flex gap-8 flex-col md:flex-row">
-                <Link href="hello" className="flex flex-row items-center gap-5">
-                  <Image
-                    src="/cold cofee.avif"
-                    alt="cold cofee"
-                    width={120}
-                    height={120}
-                    className="rounded-full"
-                  ></Image>
-                  <div>
-                    <h1 className="text-xl">Cold Coffees</h1>
-                  </div>
-                </Link>
-              </div>
+        {menuItems.map((category, index) => (
+          <div key={index} className="mt-[36px]">
+            <h1 className="font-bold text-2xl mb-4">{category.category}</h1>
+            <div className="flex flex-wrap  gap-x-[100px] border-t pt-6 gap-y-[50px]">
+              {category.items.map((item, idx) => (
+                <div key={idx} className="flex gap-8 flex-col md:flex-row">
+                  <Link
+                    href={item.link}
+                    className="flex flex-row items-center gap-5"
+                  >
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width={120}
+                      height={120}
+                      className="rounded-full"
+                    />
+                    <div>
+                      <h1 className="text-xl">{item.name}</h1>
+                    </div>
+                  </Link>
+                </div>
+              ))}
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
