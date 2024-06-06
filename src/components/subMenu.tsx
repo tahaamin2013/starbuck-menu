580f5acbc2cc638f0e37b61d911e22f5eb26f070
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-07 02:08:25 +0500

    fds

--- src/components/subMenu.tsx
+++ src/components/subMenu.tsx
@@ -1,10 +1,36 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SubMenu = () => {
+  const [rounded, setRounded] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 1) {
+        setRounded(true);
+      } else {
+        setRounded(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="border-2 bg-[#f9f9f9] px-9 py-3 sticky top-0 z-40">
-      <div className="flex gap-8 font-bold text-sm w-full justify-center sm:justify-start">
+    <div
+      className={`border-2 text-center transition-all duration-500 bg-[#f9f9f9] px-9 py-3 sticky top-0 z-40 ${
+        rounded ? "rounded-full mx-auto top-[10px] w-fit" : ""
+      }`}
+    >
+      <div
+        className={`flex gap-8 transition-all duration-500 font-bold text-sm w-full justify-center sm:justify-start ${
+          rounded ? " w-fit sm:justify-center" : ""
+        }`}
+      >
         <Link href="/">Menu</Link>
         <Link href="/featured">Featured</Link>
       </div>
