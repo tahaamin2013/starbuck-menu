72660fc782152d6c5d27b06422ddc2df8174d0ee
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-13 01:41:44 +0500

    fds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
     ),
   })).filter((category) => category.items.length > 0);
 
-  const handleSearch = (event) => {
+  const handleSearch = (event: any) => {
     const query = event.target.value.toLowerCase();
     setSearchQuery(query);
     if (query === "") {
@@ -155,14 +155,14 @@ export default function Home() {
   );
 }
 
-function CategorySection({ category }) {
+function CategorySection({ category }: any) {
   return (
     <div className="mt-[36px]">
       <h1 className="font-bold text-2xl mb-4" id={category.category}>
         {category.category}
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 border-t pt-6 gap-y-[50px]">
-        {category.items.map((item, idx) => (
+        {category.items.map((item: any, idx: any) => (
           <AnimatedLink key={idx} item={item} delay={idx * 0.1} />
         ))}
       </div>
@@ -170,14 +170,14 @@ function CategorySection({ category }) {
   );
 }
 
-function CategoryWithItems({ category }) {
+function CategoryWithItems({ category }: any) {
   return (
     <div className="mt-[36px]">
       <h1 className="font-bold text-2xl mb-4" id={category.category}>
         {category.category}
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[50px] w-full bg-blue pt-6 gap-y-[50px]">
-        {category.items.map((item, idx) => (
+        {category.items.map((item: any, idx: any) => (
           <AnimatedLink key={idx} item={item} delay={idx * 0.1} />
         ))}
       </div>
@@ -185,7 +185,7 @@ function CategoryWithItems({ category }) {
   );
 }
 
-function AnimatedLink({ item, delay }) {
+function AnimatedLink({ item, delay }: any) {
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
