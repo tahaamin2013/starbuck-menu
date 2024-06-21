833109b51c9c6b6775afb3d658b02af55c001778
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-21 16:55:25 +0500

    dsf

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import {withContentlayer} from 'next-contentlayer'
+const {withContentlayer} = require("next-contentlayer")
 
 const nextConfig = {
     compiler:{
@@ -8,4 +8,4 @@ const nextConfig = {
     }
 };
 
-export default withContentlayer({ ...nextConfig });
+module.exports = withContentlayer({ ...nextConfig });
