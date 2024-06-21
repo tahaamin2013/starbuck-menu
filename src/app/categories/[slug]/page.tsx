5541998a570743001f34b3773230e88b0c30411c
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-21 16:56:16 +0500

    fds

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import { allBlogs, Blog } from "@/.contentlayer/generated";
 import Search from "@/src/components/Blog/search";
-import Footer from "@/src/components/Footer";
 import GithubSlugger from "github-slugger";
 
 const slugger = new GithubSlugger();
@@ -41,7 +40,6 @@ const CategoryPage = ({ params }: { params: { slug: string } }) => {
   return (
     <article className="mt-6 flex gap-5 flex-col text-white px-5 sm:px-10 md:px-24 sxl:px-32">
       <Search parmy={params} />
-      <Footer />
     </article>
   );
 };
