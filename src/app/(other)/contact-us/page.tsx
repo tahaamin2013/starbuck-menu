f814258c9a08e090ab29b8af8172e34ccbf1a45e
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-21 18:47:32 +0500

    dsf

--- src/app/(other)/contact-us/page.tsx
+++ src/app/(other)/contact-us/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import React, { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-
+import { Input } from "@/src/components/ui/input";
+import { Label } from "@/src/components/ui/label";
+import { Textarea } from "@/src/components/ui/textarea";
+import { Button } from "@/src/components/ui/button";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
